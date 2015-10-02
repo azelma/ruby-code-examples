244b9feee85a718ea8523631c70fcb485227e26e
Valerie Woolard <valerie.woolard@slate.com>
2015-10-02 15:43:39 -0400

    Code examples from Ruby class 2

---
+++ countdown.rb
@@ -0,0 +1,6 @@
+# A loop that counts down from 10.
+i = 10
+while i > 0
+  puts "i is #{i}"
+  i =  i - 1
+end
---
+++ square.rb
@@ -0,0 +1,4 @@
+# A method that computes the square of a number
+def square(num)
+  num * num
+end
